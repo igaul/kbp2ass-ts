c58e81ca7bb62d0b3f5c2af8666cafeeb59a4056
i <gaul@pdx.edu>
2023-06-22 14:18:57 -0700

    convert to ts and flatten deps

--- src/index.ts
+++ src/index.ts
@@ -1,127 +1,178 @@
 #!/usr/bin/env node
-import { asyncExists, asyncReadFile, clone, msToAss } from './utils';
-import KBPParser from './kbp';
-import stringify from 'ass-stringify';
-import { StyleElement, SyllabesConfig } from './types';
-import ass = require('./assTemplate');
-
-function generateASSLine(line: any, options: SyllabesConfig) {
-	const ASSLine = [];
-	let startMs = line.start;
-	const stopMs = line.end + 100;
-	line.syllables.forEach((syl: any) => ASSLine.push(
-		(syl.text.startsWith(' ') && ASSLine.length > 0 ? ' ' : '')
-		+ '{\\k'
-		+ getProgressive(syl, options)
-		+ Math.floor(syl.duration / 10)
-		+ '}'
-		+ syl.text.trim()
-		+ (syl.text.endsWith(' ') ? ' ' : '')
-	));
-	const dialogue = clone(ass.dialogue);
-	const comment = clone(ass.dialogue);
-	dialogue.value.Start = msToAss(startMs - 900 < 0 ? 0 : startMs - 900);
-	comment.value.Start = msToAss(startMs);
-	dialogue.value.End = msToAss(stopMs);
-	comment.value.End = msToAss(stopMs);
-	dialogue.value.Text = '{\\k' + (startMs - 900 < 0 ? (900 - startMs) / 10 : 100) + ass.dialogueScript + ASSLine.join('');
-	dialogue.value.Effect = 'fx';
-	dialogue.value.Style = line.currentStyle;
-	comment.value.Text = ASSLine.join('');
-	comment.value.Effect = 'karaoke';
-	comment.key = 'Comment';
-	comment.value.Style = line.currentStyle;
-	return {
-		dialogue,
-		comment
-	};
-}
-
-function getProgressive(syl: any, options: SyllabesConfig) {
-	return Math.floor(syl.duration / 10) > Math.floor(options.minimum_progression_duration / 10) ? 'f' : '';
-}
-
-function sortStartTime(a: any, b: any) {
-	if (a.value.Start < b.value.Start) return -1;
-	if (a.value.Start > b.value.Start) return 1;
-	return 0;
-}
-
-function getStyleAss(style: StyleElement) {
-	return {
-		key: 'Style',
-		value: {
-			'Name': 'Default',
-			'Fontname': 'Arial',
-			'Fontsize': '24',
-			'PrimaryColour': '&H00FFFFFF',
-			'SecondaryColour': '&H000088EF',
-			'OutlineColour': '&H00000000',
-			'BackColour': '&H00666666',
-			'Bold': '-1',
-			'Italic': '0',
-			'Underline': '0',
-			'StrikeOut': '0',
-			'ScaleX': '100',
-			'ScaleY': '100',
-			'Spacing': '0',
-			'Angle': '0',
-			'BorderStyle': '1',
-			'Outline': '1.5',
-			'Shadow': '0',
-			'Alignment': '8',
-			'MarginL': '0',
-			'MarginR': '0',
-			'MarginV': '20',
-			'Encoding': '1',
-			...style
-		}
-	};
-}
-
-/** Convert KBP data (txt) to ASS */
-export function convertToASS(time: string, options: SyllabesConfig): string {
-	const kbp = new KBPParser(options);
-	const kara = kbp.parse(time);
-	const dialogues = [];
-	const comments = [];
-
-	const styles = clone(ass.styles);
-	styles.body = styles.body.concat(kbp.styles.length > 0 ? kbp.styles.map(style => getStyleAss(style)) :  [ass.defaultStyle]);
-	const script = clone(ass.dialogue);
-	script.value.Effect = ass.scriptFX;
-	script.value.Text = ass.script;
-	script.key = 'Comment';
-	comments.push(script);
-	for (const line of kara.track) {
-		const ASSLines = generateASSLine(line, options);
-		comments.push(clone(ASSLines.comment));
-		dialogues.push(clone(ASSLines.dialogue));
-	}
-	comments.sort(sortStartTime);
-	dialogues.sort(sortStartTime);
-	const events = clone(ass.events);
-	events.body = events.body.concat(comments, dialogues);
-	return stringify([ass.scriptInfo, styles, events]);
-}
+import  yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { readFileSync } from "node:fs";
+import { convertToASS } from "./ass";
 
 async function mainCLI() {
-	if (!process.argv[2]) {
-		throw `KBP2ass - Convert KBP karaoke to ASS file
-		Usage: kbp2ass myfile.txt
-		Output goes to stdout
-		`;
-	}
-	const txtFile = process.argv[2];
+  // Per yargs docs, use of terminalWidth() in typescript requires workaround
+  // of assigning a variable name to the instance so it can be referenced
+  const yargsInstance = yargs(hideBin(process.argv));
 
-	if (process.argv[3] && isNaN(parseInt(process.argv[3]))) throw 'minimum_progression_duration is not a number';
-	const minimum_progression_duration = process.argv[3] ? parseInt(process.argv[3]) : 1000;
+  const argv = yargsInstance
+    .scriptName("kbp2ass")
+    .parserConfiguration({
+      "camel-case-expansion": false,
+      "duplicate-arguments-array": false,
+      "strip-aliased": true,
+    })
+    // Unable to use .positional handling because yargs inexplicably treats "$0 foo bar" differently from "$0 -- foo bar"
+    .usage(
+      `$0 [options] [--] [infile [outfile]]
+						$0 [options] infile minimum-progression-duration [--] [outfile] 
 
-	if (!await asyncExists(txtFile)) throw `File ${txtFile} does not exist`;
-	const txt = await asyncReadFile(txtFile, 'utf8');
-	return convertToASS(txt, { syllable_precision: true, minimum_progression_duration });
+						Convert file from KBS project format (.kbp) to SubStation Alpha subtitle (.ass)
+
+						infile:  input file in .kbp format (stdin if not specified)
+						outfile: output file in .ass format (stdout if not specified)
+
+						For compatibility with older releases, minimum-progression-duration can be specified as a positional parameter instead of an option (if both are specified, the positional wins). If your output file name happens to be a number, use -- at some point before the second positional parameter to disable this functionality.
+
+						Disable any boolean option with --no-[option]`
+    )
+    // Used for compatibility with old syntax allowing minimum-progression-duration as a positional
+    // .positional only includes items before --, so this is how to tell if the second argument is before -- or not
+    .command("* [compat1] [compat2]", false, function (yargs) {
+      yargs
+        .positional("compat1", {
+          type: "string",
+        })
+        .positional("compat2", {
+          type: "string",
+        });
+    })
+    // Despite the fact that the second argument to .command is false,
+    // positionals on the "default" command are still shown unless explicitly hidden
+    .hide("compat1")
+    .hide("compat2")
+    .options({
+      "syllable-precision": {
+        alias: "s",
+        description:
+          "Highlight syllables individually instead of combining lines into a single string. Disabling this is not recommended.",
+        type: "boolean",
+        default: true,
+        // "nargs: 0" alone doesn't seem to be enough to stop a flag from consuming a parameter next to it
+        requiresArg: false,
+        nargs: 0,
+      },
+      "minimum-progression-duration": {
+        alias: ["m", "wipe-threshold"],
+        description:
+          "Set threshold of syllable display time in milliseconds before using progressive wipe effect (implicit default 1000)",
+        type: "number",
+        requiresArg: true,
+      },
+      "full-mode": {
+        alias: "f",
+        description:
+          'Enable processing of all positional and style information in the KBS project file (-w, -p, -b, -c). To unset any particular options use --no-{option}. For example, to run in full mode but with no border set, use "-f --no-b" or "--full-mode --no-border".',
+        type: "boolean",
+        requiresArg: false,
+        nargs: 0,
+      },
+      cdg: {
+        alias: "c",
+        description:
+          "Set the virtual resolution of the destination file to that of CDG graphics, enabling positioning, alignment, and font size to work as they do in KBS.",
+        type: "boolean",
+        requiresArg: false,
+        nargs: 0,
+      },
+      wipe: {
+        alias: "w",
+        description:
+          "Use wipe setting from project file (progressive wipe effect unless wiping is set to word by word). Sets -m to 0 if not otherwise set.",
+        type: "boolean",
+        requiresArg: false,
+        nargs: 0,
+      },
+      position: {
+        alias: "p",
+        description:
+          "Use position data from project file. This includes alignment as well as vertical/horizontal offset. Strongly recommended to use with -c option.",
+        type: "boolean",
+        requiresArg: false,
+        nargs: 0,
+      },
+      border: {
+        alias: "b",
+        description:
+          "Use default CDG border (12 pixels from top of screen). If -c option is used, these are virtual pixels. To use a custom border, set --no-border and add a border in your video editor of choice.",
+        type: "boolean",
+        requiresArg: false,
+        nargs: 0,
+      },
+    })
+    .strictOptions()
+    .check(function (argv) {
+      // Setting the type only makes it parse it as a number, it doesn't validate the result
+      if (isNaN(argv["minimum-progression-duration"])) {
+        throw new Error("--minimum-progression-duration must be a number");
+      } else if (argv._.length > 2) {
+        throw new Error(
+          "Maximum of 2 files may be specified (infile and outfile)"
+        );
+      } else {
+        return true;
+      }
+    })
+    .middleware(function (argv) {
+      if (argv["full-mode"]) {
+        argv = {
+          wipe: true,
+          position: true,
+          border: true,
+          cdg: true,
+          ...argv,
+        };
+      }
+      if (argv.wipe) {
+        argv["minimum-progression-duration"] = 0;
+      }
+      if ("compat2" in argv) {
+        if (isNaN(parseInt(argv.compat2))) {
+          argv._.unshift(argv.compat2);
+        } else {
+          argv["minimum-progression-duration"] = parseInt(argv.compat2);
+        }
+        delete argv.compat2;
+      }
+      if ("compat1" in argv) {
+        argv._.unshift(argv.compat1);
+        delete argv.compat1;
+      }
+      // "default" functionality from yargs cannot be used because it doesn't show whether a user set the value or the default set it
+      const default_opts = ["wipe", "position", "border", "cdg", "full-mode"];
+      for (let x in default_opts) {
+        const opt = default_opts[x];
+        if (!(opt in argv)) argv[opt] = false;
+      }
+      if (!("minimum-progression-duration" in argv)) {
+        argv["minimum-progression-duration"] = 1000;
+      }
+      return argv;
+    }, true)
+    .wrap(yargsInstance.terminalWidth()).argv;
+
+  let infile = argv._.shift() || "-";
+  const outfile = argv._.shift() || "-";
+
+  delete argv._;
+  delete argv["$0"];
+
+  // This should be updated to work on Windows, but it would involve some extra
+  // work because even though readFile can take a file descriptor, it doesn't seem
+  // to work as expected with process.stdin.fd
+  if (infile === "-") infile = "/dev/stdin";
+
+  const txt = readFileSync(infile, "utf8");
+
+  return convertToASS(txt, argv);
+  console.log(outfile);
 }
 
-if (require.main === module) mainCLI()
-	.then(data => console.log(data))
-	.catch(err => console.log(err));
+if (require.main === module)
+  mainCLI()
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,21 @@
-export interface SyllabesConfig {
-	syllable_precision?: boolean;
-	minimum_progression_duration?: number;
-}
-
-interface StyleElement {
+interface IConverterConfig {
+	"full-mode"?: boolean;
+	"syllable-precision"?: boolean;
+	wipe?: boolean;
+	position?: boolean;
+	border?: boolean;
+	cdg?: boolean;
+	"minimum-progression-duration"?: number;
+  }
+  
+  interface IStyle {
 	Name: string;
 	PrimaryColour: string;
 	SecondaryColour: string;
 	OutlineColour: string;
 	BackColour: string;
 	Fontname?: string;
-	Fontsize?: string;
+	Fontsize?: number;
 	Bold?: number;
 	Italic?: number;
 	Underline?: number;
@@ -19,7 +24,80 @@ interface StyleElement {
 	Outline?: number;
 	Shadow?: number;
 	Alignment?: number;
-}
-
-export function convertToASS(time: string, options: SyllabesConfig): string
-
+	ScaleX?: number;
+	ScaleY?: number;
+	Spacing?: number;
+	Angle?: number;
+	BorderStyle?: number;
+	MarginL?: number;
+	MarginR?: number;
+	MarginV?: number;
+  }
+  interface IStyleKV {
+	key: "Style";
+	value: IStyle;
+  }
+  interface IStyles {
+	section: string;
+	body: Array<{ key: "Format"; value: Array<keyof IStyle> } | IStyleKV>;
+  }
+  interface IDialogue {
+	Layer: string;
+	Start: string;
+	End: string;
+	Style: string;
+	Name: string;
+	MarginL: string;
+	MarginR: string;
+	MarginV: string;
+	Effect: string;
+	Text: string;
+  }
+  interface IDialogueKV<T> {
+	key: T;
+	value: IDialogue;
+  }
+  interface IFormatKV {
+	key: "Format";
+	value: Array<keyof IDialogue>;
+  }
+  
+  // ? same as sentence ?
+  interface ISyllable {
+	text: string;
+	start: number;
+	end: number;
+	duration: number;
+	wipeProgressive: boolean;
+  }
+  interface ISentence {
+	id: number;
+	syllables: ISyllable[];
+	start: number;
+	end: number;
+	currentStyle: string;
+	vpos: number;
+	hpos: number;
+	alignment: number;
+	text: string;
+	duration: number;
+  }
+  
+  interface IScriptInfo {
+	section: string;
+	body: {
+	  type?: string;
+	  value: string | number;
+	  key?: string;
+	}[];
+  }
+  
+  interface IEvents {
+	section: "Events";
+	body: Array<IFormatKV | IDialogueKV<"Dialogue"> | IDialogueKV<"Comment">>;
+  }
+  
+  type TSection = IScriptInfo | IStyles | IEvents;
+  
+  export function convertToASS(time: string, options: IConverterConfig): string;
+  
